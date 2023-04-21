dcbc5ee9a570ebc69db2c07728bcddcf83b02dcb
mohamed <addyenter@gmail.com>
2023-04-21 09:42:33 -0500

    dfg

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,37 @@
 
-// var query = '3lb carrots and a chicken sandwich'
-// $.ajax({
-//     method: 'GET',
-//     url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
-//     headers: { 'X-Api-Key': 'gF78d3xpyTjWRcRmQlGJTg==ElsLxWJ8t8OFYxFd'},
-//     contentType: 'application/json',
-//     success: function(result) {
-//         console.log(result);
-//     },
-//     error: function ajaxError(jqXHR) {
-//         console.error('Error: ', jqXHR.responseText);
-//     }
-// });
-
 
 const apiKey = 'gF78d3xpyTjWRcRmQlGJTg==ElsLxWJ8t8OFYxFd';
-const query = 'carrots';
+let query = 'carrots';
+
+let searchbar = document.querySelector('#searchbar');
+let searchbutton = document.querySelector('#searchbutton')
+let outputElement = document.querySelector('#output');
 
 
-fetch(`https://api.calorieninjas.com/v1/nutrition?query=${query}`, {
-  headers: {
-    'X-Api-Key': apiKey
-  }
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Request failed with status code ${response.status}`);
+searchbutton.addEventListener('click', ()=>{
+  console.log(searchbar.value);
+  query = searchbar.value;
+  console.log(query);
+
+  
+  fetch(`https://api.calorieninjas.com/v1/nutrition?query=${query}`, {
+    headers: {
+      'X-Api-Key': apiKey
     }
-    return response.json();
   })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => console.error(error));
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Request failed with status code ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      outputElement.innerHTML = `Calories: ${data.items[0].calories}`;
+      
+    })
+    .catch(error => console.error(error));
+     
+  
+
+})
